604b958907bb724133c2610e143ec328fdad739e
张树启 <673373004@qq.com>
2017-02-16 21:24:36 +0800

    .

--- app/src/main/java/com/cmax/bodysheild/api/Url.java
+++ app/src/main/java/com/cmax/bodysheild/api/Url.java
@@ -4,13 +4,13 @@ package com.cmax.bodysheild.api;
  * Created by Administrator on 2016/11/24 0024.
  */
 public class Url {
-    public static final String BASE_URL = "http://168.235.84.41:8080/bodyshield/";
-    public static final String LOGIN = "ts/user/login.json";
-    public static final String REGISTER = "ts/user/register.json";
-    public static final String IS_REGISTER = "ts/user/checkName.json";
-    public static    String UPLOAD_HEADIMAGE ="/ts/user/upload/headImg.json";
-    public static final String FEEDBACK ="/ts/user/feedback.json";
-    public static final String UPLOAD_TEMPERATURE = "/ts/temperature/upload/data.json";
-    public static final String DOWNLOAD_TEMPERATURE = "/ts/temperature/load/data.json";
+    public static final String BASE_URL = "http://api.apexto.com:8080/bodyshield/";
+    public static final String LOGIN = "user/login.json";
+    public static final String REGISTER = "user/register.json";
+    public static final String IS_REGISTER = "user/checkName.json";
+    public static    String UPLOAD_HEADIMAGE ="user/upload/headImg.json";
+    public static final String FEEDBACK ="user/feedback.json";
+    public static final String UPLOAD_TEMPERATURE = "temperature/upload/data.json";
+    public static final String DOWNLOAD_TEMPERATURE = "temperature/load/data.json";
     public static final String THIRD_LOGIN="user/third/login.json";
 }
